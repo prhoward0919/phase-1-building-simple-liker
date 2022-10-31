26c42453ffb4ba18d9cf4513afe8e98d58408b87
Paige <prhoward97@gmail.com>
2022-10-31 13:29:56 -0400

    commit tree ff6c0879c45e45b371c2f81643ad7de77a3df11d

--- main.js
+++ main.js
@@ -5,6 +5,35 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+document.addEventListener("DOMContentLoaded", () => {activeHeart()})
+
+//const modal =  document.querySelectorAll('#modal');
+//console.log(modal);
+//modal.classList.add('hidden');
+
+const heart = document.querySelectorAll('span.like-glyph');
+heart.forEach(activeHeart);
+
+
+function activeHeart(hearts) {
+  hearts.addEventListener('click', (e) => {
+    mimicServerCall()
+    .then((response) => {
+      if (e.target.innerText === EMPTY_HEART) {
+        e.target.classList.add('activated-heart')
+        e.target.innerText = FULL_HEART
+      }
+      else {
+        e.target.classList.remove('activated-heart');
+        e.target.innerText = EMPTY_HEART;
+      }
+    })
+    .catch(function() {
+      document.getElementById('modal').classList.remove('hidden');
+      setTimeout(() => {document.getElementById('modal').classList.add('hidden')}, 3000);
+    })
+  })
+  
 
 
 //------------------------------------------------------------------------------
@@ -22,4 +51,4 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
       }
     }, 300);
   });
-}
+}}
